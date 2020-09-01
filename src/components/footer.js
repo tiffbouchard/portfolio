288b761789bc3ad9e3b671a95969b073bfba7ff5
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-08-31 21:01:12 -0400

    make footer

---
+++ src/components/footer.js
@@ -0,0 +1,55 @@
+import React from "react"
+import styled from "styled-components"
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  list-style-type: none;
+  display: flex;
+  justify-content: space-around;
+  position: fixed-bottom;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding-top: 15px;
+  background-color: #f0faf1;
+    a {
+      padding: 10px;
+      font-family: NMBold;
+      background: none;
+      font-size: 45px;
+      border: none;
+      color: black;
+      transition: 0.5s;
+      text-decoration: none;
+      -webkit-text-fill-color: #f0faf1;
+      -webkit-text-stroke-width: 1.3px;
+      -webkit-text-stroke-color: black;
+      &:hover {
+        text-shadow: 2px 2px;
+        transition-duration: 0.5s;
+      }
+      @media (max-width: 950px) {
+      font-size: 50px;
+    }
+      @media (max-width: 600px) {
+        font-size: 20px;
+    }
+    @media (max-height: 650px) {
+      font-size: 20px;
+    }
+    }
+  }
+`
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <a href="#about">ABOUT</a>
+      <a href="#projects">PROJECTS</a>
+      <a href="#contact">CONTACT</a>
+      <a href="#resume">RESUME</a>
+    </FooterContainer>
+  )
+}
+
+export default Footer
