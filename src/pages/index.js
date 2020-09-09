30acbf08876885d8233875eb786b2944f39b6900
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-09-08 22:51:15 -0400

    remove 404 pg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-// import SEO from "../components/seo"
-
 import Socials from "../components/socials"
 import Nav from "../components/nav.js"
 import Footer from "../components/footer"
@@ -12,7 +10,7 @@ import Contact from "../components/sections/contact.js"
 
 import "../../src/global.css"
 
-const IndexPage = () => {
+const IndexPage = props => {
   return (
     <>
       <Socials />
